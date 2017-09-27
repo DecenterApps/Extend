ab608f9d7951d25852c7a3622e572bf39aacd227
astankovic <aleksa.stankovic@berkeley.edu>
2017-09-27 18:29:25 +0200

    Add Reddit oauth to background.js

--- app/background/src/index.js
+++ app/background/src/index.js
@@ -7,6 +7,16 @@ import { STORE_PORT } from '../../constants/general';
 const store = createStore(rootReducer, {});
 const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  const regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
 wrapStore(store, { portName: STORE_PORT });
 
 web3.version.getNetwork((err, netId) => {
@@ -50,4 +60,34 @@ const pollForAddressChange = () => {
   }, 100);
 };
 
-pollForAddressChange();
+var redirectUri = chrome.identity.getRedirectURL("oauth2");
+
+console.log("REDIRECT URI", redirectUri);
+
+chrome.identity.launchWebAuthFlow({
+  url: `https://www.reddit.com/api/v1/authorize?client_id=AFH0yVxKuLUlVQ&response_type=token&state=asdf&redirect_uri=${redirectUri}&scope=identity`,
+  interactive: true
+}, (response) => {
+  response = response.replace(/#/g, "?");
+  const accessToken = getParameterByName('access_token', response);
+
+  console.log(accessToken);
+
+  const request = new Request('https://oauth.reddit.com/api/v1/me', {
+    headers: new Headers({
+      "Authorization": `bearer ${accessToken}`
+    })
+  });
+
+  fetch(request)
+    .then(function(response) {
+        console.log('RESPONSE', response);
+        response.json().then((user) => {
+          const username = user.name;
+          console.log(username);
+        })
+    })
+    .catch((err) => {
+      console.log('ERROR reading user', err)
+    });
+});

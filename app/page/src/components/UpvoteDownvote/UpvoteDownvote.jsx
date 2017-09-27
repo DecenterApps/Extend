ec2df3b8bfcde8dba4eab5c9ebc577ba05e424cd
astankovic <aleksa.stankovic@berkeley.edu>
2017-09-27 12:53:10 +0200

    Add dispatch, fix states

--- app/page/src/components/UpvoteDownvote/UpvoteDownvote.jsx
+++ app/page/src/components/UpvoteDownvote/UpvoteDownvote.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import UpIcon from '../Decorative/UpIcon/UpIcon'
-import DownIcon from '../Decorative/DownIcon/DownIcon'
+import UpIcon from '../Decorative/UpIcon/UpIcon';
+import DownIcon from '../Decorative/DownIcon/DownIcon';
+import { count } from '../../../../actions/voteActions';
 
-const UpvoteDownvote = ({count}) => (
+const UpvoteDownvote = ({count, onClick}) => (
   <div>
-    <span><UpIcon /></span>
+    <span onClick={ onClick }><UpIcon /></span>
+    <span>{ count }</span>
     <span><DownIcon /></span>
   </div>
 );
 
+UpvoteDownvote.defaultProps = {
+  count: 0
+};
 
 const mapStateToProps = (state) => ({
   count: state.count
 });
 
-export default connect(mapStateToProps)(UpvoteDownvote);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onClick: () => { dispatch(count()); }
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(UpvoteDownvote);
--- app/page/src/index.js
+++ app/page/src/index.js
@@ -1,7 +1,8 @@
-import {Store} from 'react-chrome-redux';
+import { Store } from 'react-chrome-redux';
+import { STORE_PORT } from '../../constants/general';
 import insertUpvoteDownvote from './components/UpvoteDownvote/insertUpvoteDownvote';
 
-const proxyStore = new Store({portName: 'example'});
+const proxyStore = new Store({portName: STORE_PORT});
 
 insertUpvoteDownvote(proxyStore);
 

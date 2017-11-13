b23236b689c3b42b12a6b27ccaced2f8205a74d3
Nikola Klipa <klipa.nikola@gmail.com>
2017-11-13 16:23:55 +0100

    Code refactor, change name to Extend

--- solidity/deploy.sh
+++ solidity/deploy.sh
@@ -1,2 +1,4 @@
 set -e # fail if any deployment fails
-dapp create Reddapp --from=0x6c259ea1fCa0D1883e3FFFdDeb8a0719E1D7265f
+events=$(ETH_GAS=1061880 dapp create ExtendEvents --from=0x6c259ea1fCa0D1883e3FFFdDeb8a0719E1D7265f)
+data=$(ETH_GAS=1277429 dapp create ExtendData --from=0x6c259ea1fCa0D1883e3FFFdDeb8a0719E1D7265f)
+ETH_GAS=4764138 dapp create Extend "$data" "$events" --from=0x6c259ea1fCa0D1883e3FFFdDeb8a0719E1D7265f

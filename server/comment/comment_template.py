d13072d8367b11873122ec2ccef8a1c270dbf0c5
nebojsa.urosevic <nebojsa94urosevic@gmail.com>
2018-02-04 15:47:11 +0100

    Template refactor.

--- server/comment/comment_template.py
+++ server/comment/comment_template.py
@@ -1,23 +1,14 @@
 USER_TIPPED = """I'm a bot, bleep, bloop. 
 I just want to notify you that
 {from_username} has sent you a tip of {ethAmount} ETH via
-ΞXTΞND, a Chrome browser extension that creates a bridge
+[ΞXTΞND]({blog}), an [open source]({github}) Chrome browser extension that creates a bridge
 between popular websites such as Reddit and Ethereum blockchain. 
-You can claim this tip by installing the extension from Chrome Web Store.
-ΞXTΞND is completely open source, safe to use and
-transparent - more details can be found via links below.
-\n[View code on Github]({github})
-\n[Install it from Chrome Web Store]({webstore})
-\n[More about ΞXTΞND]({blog})"""
+You can claim this tip by installing the extension from [Chrome Web Store]({webstore})."""
 
 BOUGHT_GOLD = """I'm a bot, bleep, bloop. 
 I just want to notify you that
 {from_username} has bought you {months} of reddit gold via
-ΞXTΞND, a Chrome browser extension that creates a bridge
+[ΞXTΞND]({blog}), an [open source]({github}) [Chrome browser extension]({webstore}) that creates a bridge
 between popular websites such as Reddit and Ethereum blockchain. 
 You don't need to do anything in order to claim this reward,
-but we thought you might want to learn more about ΞXTΞND,
-so we are including some relevant links below.
-\n[View code on Github]({github})
-\n[Install it from Chrome Web Store]({webstore})
-\n[More about ΞXTΞND]({blog})"""
+but we thought you might want to learn more about ΞXTΞND."""
